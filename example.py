a252a24469d3cee464b532c0417fa862b10246c6
Aaro Altonen <a.altonen@hotmail.com>
2023-01-09 14:21:08 +0200

    Add example python code

---
+++ example.py
@@ -0,0 +1,13 @@
+
+def spawn_interfaces():
+    # TODO: spawn n sybil nodes and connect them to `swarm-host`
+
+def spawn_nodes():
+    # TODO: spawn n sybil nodes and connect them to `swarm-host`
+
+def setup():
+    self.spawn_interfaces()
+    self.spawn_nodes()
+
+def main():
+    # TODO: describe some attack here
