3847277e796a4a743fe93888f251282a9e3295f0
Aaro Altonen <a.altonen@hotmail.com>
2023-04-13 21:21:39 +0300

    backend/substrate: Convert `PeerId` to string
    There are probably better formats but this a type that Python can use
    as a dictionary key

--- src/backend/substrate/mod.rs
+++ src/backend/substrate/mod.rs
@@ -354,7 +354,7 @@ impl IntoExecutorObject for <SubstrateBackend as NetworkBackend>::PeerId {
     type Context<'a> = pyo3::marker::Python<'a>;
 
     fn into_executor_object(self, context: Self::Context<'_>) -> Self::NativeType {
-        self.0.to_bytes().into_py(context)
+        self.0.to_base58().into_py(context)
     }
 }
 
