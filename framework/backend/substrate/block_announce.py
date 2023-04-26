bf0ed771a99bb0c2ed6ab87b777790f06509ab45
Aaro Altonen <a.altonen@hotmail.com>
2023-04-26 21:02:40 +0300

    framework: Add Python helper code for `/block-announces/1`

---
+++ framework/backend/substrate/block_announce.py
@@ -0,0 +1,68 @@
+from scalecodec.base import RuntimeConfiguration, ScaleBytes
+from scalecodec.type_registry import load_type_registry_preset
+
+import hashlib
+
+class BlockAnnounce():
+    def __init__(self, notification):
+        RuntimeConfiguration().update_type_registry(load_type_registry_preset("legacy"))
+        RuntimeConfiguration().update_type_registry({
+            "types": {
+                "BlockAnnounce": {
+                    "type": "struct",
+                    "type_mapping": [
+                        [
+                            "header",
+                            "Header",
+                        ],
+                        [
+                            "state",
+                            "BlockState",
+                        ],
+                        [
+                            "data",
+                            "Option<Vec<u8>>",
+                        ],
+                    ],
+                },
+                "BlockState": {
+                    "type": "enum",
+                    "type_mapping": [
+                        [
+                            "Normal",
+                            "Null",
+                        ],
+                        [
+                            "Best",
+                            "Null",
+                        ],
+                    ]
+                },
+            }
+        })
+
+        self.announce = RuntimeConfiguration().create_scale_object(
+            'BlockAnnounce',
+            data = ScaleBytes(notification),
+        )
+        self.announce.decode()
+
+    """
+        Get block header.
+    """
+    def header(self):
+        return self.announce['header']
+
+    """
+        Calculate block hash
+    """
+    def hash(self):
+        header = RuntimeConfiguration().create_scale_object(
+            'Header',
+        )
+        encoded = header.encode(self.announce['header'])
+        byte_string = bytes.fromhex(encoded.to_hex()[2:])[:-2] # TODO: zzz
+        hash_object = hashlib.blake2b(digest_size = 32)
+        hash_object.update(byte_string)
+
+        return '0x' + str(hash_object.digest().hex())
--- framework/block-announce-filter.py
+++ framework/block-announce-filter.py
@@ -7,63 +7,16 @@ from scalecodec.base import RuntimeConfiguration, ScaleBytes
 import base64
 import hashlib
 
+from backend.substrate.block_announce import BlockAnnounce
+
 def filter_notification(
     ctx,
     peer,
     notification
 ):
-    # TODO: move this somewhere else?
-    RuntimeConfiguration().update_type_registry(load_type_registry_preset("legacy"))
-    RuntimeConfiguration().update_type_registry({
-        "types": {
-            "BlockAnnounce": {
-                "type": "struct",
-                "type_mapping": [
-                    [
-                        "header",
-                        "Header",
-                    ],
-                    [
-                        "state",
-                        "BlockState",
-                    ],
-                    [
-                        "data",
-                        "Option<Vec<u8>>",
-                    ],
-                ],
-            },
-            "BlockState": {
-                "type": "enum",
-                "type_mapping": [
-                    [
-                        "Normal",
-                        "Null",
-                    ],
-                    [
-                        "Best",
-                        "Null",
-                    ],
-                ]
-            },
-        }
-    })
-
     try:
-        obj = RuntimeConfiguration().create_scale_object(
-            'BlockAnnounce',
-            data = ScaleBytes(bytes(notification)),
-        )
-        obj.decode()
-        test = RuntimeConfiguration().create_scale_object(
-            'Header',
-        )
-        encoded = test.encode(obj['header'])
-        # TODO: what are these two extra bytes at the end?
-        byte_string = bytes.fromhex(encoded.to_hex()[2:])[:-2]
-        hash_object = hashlib.blake2b(digest_size=32)
-        hash_object.update(byte_string)
-        ctx.peers[peer].known_blocks.add('0x' + str(hash_object.digest().hex()))
+        block_announce = BlockAnnounce(bytes(notification))
+        ctx.peers[peer].known_blocks.add(block_announce.hash())
         return { 'Forward': None }
     except Exception as e:
         print("failed to handle block announce:", e)
