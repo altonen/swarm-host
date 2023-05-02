3ffd544153c57e27eb74df54cbce4e57da632807
Aaro Altonen <a.altonen@hotmail.com>
2023-05-02 23:21:19 +0300

    substrate/backend: Format code in `sc-network-common` and `sc-peerset`

--- src/backend/substrate/network/common/src/service/signature.rs
+++ src/backend/substrate/network/common/src/service/signature.rs
@@ -19,35 +19,35 @@
 // If you read this, you are very thorough, congratulations.
 
 use libp2p::{
-	identity::{error::SigningError, Keypair, PublicKey},
-	PeerId,
+    identity::{error::SigningError, Keypair, PublicKey},
+    PeerId,
 };
 
 /// A result of signing a message with a network identity. Since `PeerId` is potentially a hash of a
 /// `PublicKey`, you need to reveal the `PublicKey` next to the signature, so the verifier can check
 /// if the signature was made by the entity that controls a given `PeerId`.
 pub struct Signature {
-	/// The public key derived from the network identity that signed the message.
-	pub public_key: PublicKey,
-	/// The actual signature made for the message signed.
-	pub bytes: Vec<u8>,
+    /// The public key derived from the network identity that signed the message.
+    pub public_key: PublicKey,
+    /// The actual signature made for the message signed.
+    pub bytes: Vec<u8>,
 }
 
 impl Signature {
-	/// Create a signature for a message with a given network identity.
-	pub fn sign_message(
-		message: impl AsRef<[u8]>,
-		keypair: &Keypair,
-	) -> Result<Self, SigningError> {
-		let public_key = keypair.public();
-		let bytes = keypair.sign(message.as_ref())?;
-		Ok(Self { public_key, bytes })
-	}
+    /// Create a signature for a message with a given network identity.
+    pub fn sign_message(
+        message: impl AsRef<[u8]>,
+        keypair: &Keypair,
+    ) -> Result<Self, SigningError> {
+        let public_key = keypair.public();
+        let bytes = keypair.sign(message.as_ref())?;
+        Ok(Self { public_key, bytes })
+    }
 
-	/// Verify whether the signature was made for the given message by the entity that controls the
-	/// given `PeerId`.
-	pub fn verify(&self, message: impl AsRef<[u8]>, peer_id: &PeerId) -> bool {
-		*peer_id == self.public_key.to_peer_id() &&
-			self.public_key.verify(message.as_ref(), &self.bytes)
-	}
+    /// Verify whether the signature was made for the given message by the entity that controls the
+    /// given `PeerId`.
+    pub fn verify(&self, message: impl AsRef<[u8]>, peer_id: &PeerId) -> bool {
+        *peer_id == self.public_key.to_peer_id()
+            && self.public_key.verify(message.as_ref(), &self.bytes)
+    }
 }
--- src/backend/substrate/network/common/src/sync/warp.rs
+++ src/backend/substrate/network/common/src/sync/warp.rs
@@ -26,86 +26,86 @@ pub struct EncodedProof(pub Vec<u8>);
 /// Warp sync request
 #[derive(Encode, Decode, Debug)]
 pub struct WarpProofRequest<B: BlockT> {
-	/// Start collecting proofs from this block.
-	pub begin: B::Hash,
+    /// Start collecting proofs from this block.
+    pub begin: B::Hash,
 }
 
 /// The different types of warp syncing.
 pub enum WarpSyncParams<Block: BlockT> {
-	/// Standard warp sync for the relay chain
-	WithProvider(Arc<dyn WarpSyncProvider<Block>>),
-	/// Skip downloading proofs and wait for a header of the state that should be downloaded.
-	///
-	/// It is expected that the header provider ensures that the header is trusted.
-	WaitForTarget(oneshot::Receiver<<Block as BlockT>::Header>),
+    /// Standard warp sync for the relay chain
+    WithProvider(Arc<dyn WarpSyncProvider<Block>>),
+    /// Skip downloading proofs and wait for a header of the state that should be downloaded.
+    ///
+    /// It is expected that the header provider ensures that the header is trusted.
+    WaitForTarget(oneshot::Receiver<<Block as BlockT>::Header>),
 }
 
 /// Proof verification result.
 pub enum VerificationResult<Block: BlockT> {
-	/// Proof is valid, but the target was not reached.
-	Partial(SetId, AuthorityList, Block::Hash),
-	/// Target finality is proved.
-	Complete(SetId, AuthorityList, Block::Header),
+    /// Proof is valid, but the target was not reached.
+    Partial(SetId, AuthorityList, Block::Hash),
+    /// Target finality is proved.
+    Complete(SetId, AuthorityList, Block::Header),
 }
 
 /// Warp sync backend. Handles retrieveing and verifying warp sync proofs.
 pub trait WarpSyncProvider<Block: BlockT>: Send + Sync {
-	/// Generate proof starting at given block hash. The proof is accumulated until maximum proof
-	/// size is reached.
-	fn generate(
-		&self,
-		start: Block::Hash,
-	) -> Result<EncodedProof, Box<dyn std::error::Error + Send + Sync>>;
-	/// Verify warp proof against current set of authorities.
-	fn verify(
-		&self,
-		proof: &EncodedProof,
-		set_id: SetId,
-		authorities: AuthorityList,
-	) -> Result<VerificationResult<Block>, Box<dyn std::error::Error + Send + Sync>>;
-	/// Get current list of authorities. This is supposed to be genesis authorities when starting
-	/// sync.
-	fn current_authorities(&self) -> AuthorityList;
+    /// Generate proof starting at given block hash. The proof is accumulated until maximum proof
+    /// size is reached.
+    fn generate(
+        &self,
+        start: Block::Hash,
+    ) -> Result<EncodedProof, Box<dyn std::error::Error + Send + Sync>>;
+    /// Verify warp proof against current set of authorities.
+    fn verify(
+        &self,
+        proof: &EncodedProof,
+        set_id: SetId,
+        authorities: AuthorityList,
+    ) -> Result<VerificationResult<Block>, Box<dyn std::error::Error + Send + Sync>>;
+    /// Get current list of authorities. This is supposed to be genesis authorities when starting
+    /// sync.
+    fn current_authorities(&self) -> AuthorityList;
 }
 
 /// Reported warp sync phase.
 #[derive(Clone, Eq, PartialEq, Debug)]
 pub enum WarpSyncPhase<Block: BlockT> {
-	/// Waiting for peers to connect.
-	AwaitingPeers,
-	/// Waiting for target block to be received.
-	AwaitingTargetBlock,
-	/// Downloading and verifying grandpa warp proofs.
-	DownloadingWarpProofs,
-	/// Downloading target block.
-	DownloadingTargetBlock,
-	/// Downloading state data.
-	DownloadingState,
-	/// Importing state.
-	ImportingState,
-	/// Downloading block history.
-	DownloadingBlocks(NumberFor<Block>),
+    /// Waiting for peers to connect.
+    AwaitingPeers,
+    /// Waiting for target block to be received.
+    AwaitingTargetBlock,
+    /// Downloading and verifying grandpa warp proofs.
+    DownloadingWarpProofs,
+    /// Downloading target block.
+    DownloadingTargetBlock,
+    /// Downloading state data.
+    DownloadingState,
+    /// Importing state.
+    ImportingState,
+    /// Downloading block history.
+    DownloadingBlocks(NumberFor<Block>),
 }
 
 impl<Block: BlockT> fmt::Display for WarpSyncPhase<Block> {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		match self {
-			Self::AwaitingPeers => write!(f, "Waiting for peers"),
-			Self::AwaitingTargetBlock => write!(f, "Waiting for target block to be received"),
-			Self::DownloadingWarpProofs => write!(f, "Downloading finality proofs"),
-			Self::DownloadingTargetBlock => write!(f, "Downloading target block"),
-			Self::DownloadingState => write!(f, "Downloading state"),
-			Self::ImportingState => write!(f, "Importing state"),
-			Self::DownloadingBlocks(n) => write!(f, "Downloading block history (#{})", n),
-		}
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::AwaitingPeers => write!(f, "Waiting for peers"),
+            Self::AwaitingTargetBlock => write!(f, "Waiting for target block to be received"),
+            Self::DownloadingWarpProofs => write!(f, "Downloading finality proofs"),
+            Self::DownloadingTargetBlock => write!(f, "Downloading target block"),
+            Self::DownloadingState => write!(f, "Downloading state"),
+            Self::ImportingState => write!(f, "Importing state"),
+            Self::DownloadingBlocks(n) => write!(f, "Downloading block history (#{})", n),
+        }
+    }
 }
 
 /// Reported warp sync progress.
 #[derive(Clone, Eq, PartialEq, Debug)]
 pub struct WarpSyncProgress<Block: BlockT> {
-	/// Estimated download percentage.
-	pub phase: WarpSyncPhase<Block>,
-	/// Total bytes downloaded so far.
-	pub total_bytes: u64,
+    /// Estimated download percentage.
+    pub phase: WarpSyncPhase<Block>,
+    /// Total bytes downloaded so far.
+    pub total_bytes: u64,
 }
