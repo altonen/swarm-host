2310a7784868d8d1c0c9022ea7ba4366506f43ba
Aaro Altonen <a.altonen@hotmail.com>
2023-05-17 21:01:53 +0300

    framework: Use `PYTHONPATH` to initialize Substrate default filters

--- framework/swarm_host/backend/substrate/swarm_host.py
+++ framework/swarm_host/backend/substrate/swarm_host.py
@@ -1,5 +1,7 @@
 from swarm_host.swarm_host import SwarmHost
 
+import os
+
 class SubstrateSwarmHost(SwarmHost):
     def __init__(self, rpc_port, genesis_hash):
         super().__init__(rpc_port, "substrate", genesis_hash)
@@ -9,15 +11,29 @@ class SubstrateSwarmHost(SwarmHost):
           - `/sup/block-announces/1`
           - `/sup/sync/2`
     """
-    def create_interface(self, port, preinit = None, context = None):
+    def create_interface(
+        self,
+        port,
+        preinit = None,
+        context = None,
+        block_announce_filter = None,
+        sync_filter = None
+    ):
+        if "PYTHONPATH" not in os.environ:
+            raise "`PYTHONPATH` must be defined"
+
+        swarm_host_path = os.environ["PYTHONPATH"]
+        if swarm_host_path[:-1] != "/":
+            swarm_host_path += "/"
+
         if preinit is None:
-            preinit = open("swarm_host/backend/substrate/filters/preinit.py").read()
+            preinit = open(swarm_host_path + "swarm_host/backend/substrate/filters/preinit.py").read()
         if context is None:
-            context = open("swarm_host/backend/substrate/filters/context.py").read()
+            context = open(swarm_host_path + "swarm_host/backend/substrate/filters/context.py").read()
         self.id = super().create_interface(port, context, preinit)
 
         # install block announces filter
-        filter = open("swarm_host/backend/substrate/filters/block-announces-1.py").read()
+        filter = open(swarm_host_path + "swarm_host/backend/substrate/filters/block-announces-1.py").read()
         super().install_notification_filter(
             self.id,
             "/sup/block-announces/1",
@@ -25,7 +41,7 @@ class SubstrateSwarmHost(SwarmHost):
         )
 
         # install sync filter
-        filter = open("swarm_host/backend/substrate/filters/sup-sync-2.py").read()
+        filter = open(swarm_host_path + "swarm_host/backend/substrate/filters/sup-sync-2.py").read()
         super().install_request_response_filter(
             self.id,
             "/sup/sync/2",
